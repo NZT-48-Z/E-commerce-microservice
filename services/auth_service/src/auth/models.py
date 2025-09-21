fbb3ab1e052b5717f2fb0b0f8d69d0901294171a
NZT <adusevaleksei2@gmail.com>
2025-09-21 16:20:22 +0300

    refactor(auth-service): add __init__.py files and change lenght param to String()

--- services/auth_service/src/auth/models.py
+++ services/auth_service/src/auth/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Enum, ForeignKey
+from sqlalchemy import Enum, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from pydantic import EmailStr
 
 from auth.column_types import intpk, created_at, updated_at
 from auth.enums import UserStatus
@@ -11,9 +10,9 @@ class UsersOrm(Base):
     __tablename__ = "users"
 
     id: Mapped[intpk]
-    email: Mapped[EmailStr] = mapped_column(unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
     phone: Mapped[str] = mapped_column(
-        length=16, unique=True, index=True, nullable=True
+        String(16), unique=True, index=True, nullable=True
     )
     hashed_password: Mapped[str] = mapped_column(nullable=False)
     status: Mapped[UserStatus] = mapped_column(
@@ -33,8 +32,8 @@ class RolesOrm(Base):
     __tablename__ = "roles"
 
     id: Mapped[intpk]
-    name: Mapped[str] = mapped_column(length=50, unique=True, nullable=False)
-    description: Mapped[str] = mapped_column(length=255, nullable=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
 
     users = relationship(
         "UsersOrm", secondary="role_permissions", back_populates="roles"
@@ -45,8 +44,8 @@ class PermissionsOrm(Base):
     __tablename__ = "permissions"
 
     id: Mapped[intpk]
-    name: Mapped[str] = mapped_column(length=50, unique=True, nullable=False)
-    description: Mapped[str] = mapped_column(length=255, nullable=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
 
 
 class RolePermissionsOrm(Base):
